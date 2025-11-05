555423b228f6d71aafe4116f95a30c7d3c7dc097
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-11-05 16:57:17 +0300

    landing page being made

--- my-npm/src/App.jsx
+++ my-npm/src/App.jsx
@@ -1,11 +1,14 @@
 import Home from "./pages/Home";
 import './App.css'
+import Navbar from "./components/Navbar";
+
 
 function App() {
 
   return (
     <>
-    return <Home />;
+      <Navbar />
+    <Home />;
     </>
   )
 }
---
+++ my-npm/src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const Navbar = () => {
+  const links = [
+    "Home",
+    "About",
+    "For Partners",
+    "For Moms",
+    "Blog",
+    "Podcast",
+    "Resources",
+    "Contact",
+  ];
+
+  return (
+    <nav className="flex items-center justify-between px-8 py-4 bg-white shadow">
+      {/* Logo */}
+      <div className="text-xl font-semibold" style={{ color: "#E8C5A8" }}>
+        YourLogo
+      </div>
+
+      {/* Nav Links */}
+      <ul className="flex gap-8 text-sm font-medium">
+        {links.map((item) => (
+          <li key={item}>
+            <a
+              href="#"
+              className="relative pb-1"
+              style={{ color: "#E8C5A8" }}
+            >
+              {item}
+
+              {/* White underline that only appears on hover */}
+              <span
+                className="absolute left-0 bottom-0 w-full h-[2px] bg-white scale-x-0 transition-transform duration-300 origin-left hover:scale-x-100"
+              ></span>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
