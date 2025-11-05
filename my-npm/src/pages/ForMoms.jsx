01dc59cc6225fb820522dd95f3cc1dbeafb4b03c
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-11-06 02:28:51 +0300

    Added react router and other page templates

--- my-npm/src/pages/ForMoms.jsx
+++ my-npm/src/pages/ForMoms.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const About = () => {
+const ForMoms = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const About = () => {
   )
 }
 
-export default About
+export default ForMoms
